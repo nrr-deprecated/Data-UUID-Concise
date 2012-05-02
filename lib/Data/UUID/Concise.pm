12f0ff97e66f52a76eb596c0ff112f60ad72b8e6
Nathaniel R Reindl <nrr@corvidae.org>
2012-05-02 09:59:21 -0500

    Add a _normalize_alphabet method and set a trigger appropriately.
    When the alphabet changes, we should be sorting it lexicographically and then
    running through each element in the list to ensure that they are all distinct.

--- lib/Data/UUID/Concise.pm
+++ lib/Data/UUID/Concise.pm
@@ -12,6 +12,7 @@ use MooX::Types::MooseLike::Base qw(:all);
 
 use Carp;
 use Data::UUID;
+use List::MoreUtils qw[ uniq ];
 use Math::BigInt;
 
 # VERSION
@@ -26,13 +27,19 @@ use Math::BigInt;
     my $encoded_uuid = $duc->encode((Data::UUID->new)->create);
     my $decoded_uuid = $duc->decode('M55djt9tt4WoFaL68da9Ef');
 
+    $duc->alphabet('aaaaabcdefgh1230123');
+    $duc->alphabet; # 0123abcdefgh
+
 =cut
 
 =attr alphabet
 
-This is the collection of symbols that are used for the encoding scheme. By
-default, a reasonably unambiguous set of characters is used that is reminiscent
-of the base 58 scheme used by a rather prominent photo site's URL shortener.
+This is the collection of symbols that are used for the encoding
+scheme.
+
+By default, a reasonably unambiguous set of characters is used that is
+reminiscent of the base 58 scheme used by a rather prominent photo
+site's URL shortener.
 
 =cut
 
@@ -42,11 +49,34 @@ has 'alphabet' => (
     default => sub {
         '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
     },
+    trigger => sub {
+    	my ($self, @rest) = @_;
+    	$self->_normalize_alphabet(@rest);
+    },
 );
+
+=method _normalize_alphabet
+
+Private method. Normalize the alphabet such that it is sorted and that
+all elements are distinct.
+
+=cut
+
+sub _normalize_alphabet
+{
+    my ($self, $alphabet_candidate) = @_;
+
+    my @symbols = split //, $alphabet_candidate;
+    my @decruftified_symbols = uniq sort { $a cmp $b } @symbols;
+    my $decruftified_alphabet = join '', @decruftified_symbols;
+
+    return $self->alphabet($decruftified_alphabet);
+}
 
 =method encode
 
-Encode a Data::UUID instance as a string with the appropriate set of symbols.
+Encode a Data::UUID instance as a string with the appropriate set of
+symbols.
 
 =cut
 
@@ -70,8 +100,8 @@ sub encode
 
 =method decode
 
-Decode a string with the appropriate set of symbols and return a Data::UUID
-instance representing the decoded UUID.
+Decode a string with the appropriate set of symbols and return a
+Data::UUID instance representing the decoded UUID.
 
 =cut
 
